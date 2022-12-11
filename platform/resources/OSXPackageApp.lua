8b453f85f18e0067e1c1d8a2dcfc5a078125662a
Jochem Pouwels <pouwelsjochem@gmail.com>
2022-12-11 14:09:38 +0100

    Removed com.apple.security.app-sandbox entitlement since it can't be used with Steam

--- platform/resources/OSXPackageApp.lua
+++ platform/resources/OSXPackageApp.lua
@@ -336,8 +336,6 @@ local templateXcent = [[
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-    <key>com.apple.security.app-sandbox</key>
-    <true/>
 {{CUSTOM_ENTITLEMENTS}}
 </dict>
 </plist>
