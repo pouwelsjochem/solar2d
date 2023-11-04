7899fb3a4642b66222d708820063c2bb8a313584
pouwelsjochem <pouwelsjochem@gmail.com>
2023-11-04 15:28:19 +0100

    Also trying this < Android 11

--- platform/android/sdk/src/com/ansca/corona/CoronaActivity.java
+++ platform/android/sdk/src/com/ansca/corona/CoronaActivity.java
@@ -28,6 +28,7 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.util.Log;
 import android.widget.*;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 import com.ansca.corona.events.EventManager;
@@ -156,22 +157,10 @@ public class CoronaActivity extends Activity {
 	}
 
 	private void setSystemUiVisibility() {
-		if (android.os.Build.VERSION.SDK_INT < 19) {
-			myGLView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE); // On API Level 14 and above: View.SYSTEM_UI_FLAG_LOW_PROFILE dims any on screen buttons if they exists
-		} else if (android.os.Build.VERSION.SDK_INT >= 30) {
-			myGLView.setDecorFitsSystemWindows(false);
-
-			WindowInsetsControllerCompat controller = WindowInsetsControllerCompat(myGLView, myGLView.getDecorView());
-			controller.hide(WindowInsetsCompat.Type.systemBars());
-			controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-		} else {
-			myGLView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-					| View.SYSTEM_UI_FLAG_FULLSCREEN
-					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-		}
+		WindowCompat.setDecorFitsSystemWindows(myGLView, false);
+		WindowInsetsControllerCompat controller = WindowInsetsControllerCompat(myGLView, myGLView.getDecorView());
+		controller.hide(WindowInsetsCompat.Type.systemBars());
+		controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
 	}
 
 	/**
