f1431fde29173a672cadc16b3266ac6cf4f2570a
Jochem Pouwels <pouwelsjochem@gmail.com>
2025-07-11 21:28:55 +0200

    Trying out fix for 2022 runner

--- platform/windows/Build.Tools/VSVars.bat
+++ platform/windows/Build.Tools/VSVars.bat
@@ -1,16 +1,17 @@
 @echo off
 
 echo Setup command line
-for %%V in ("%VS120COMNTOOLS%vsvars32.bat"
-           ,"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-           ,"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" 
-           ) do (
+for %%V in (
+    "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars32.bat"
+    "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+) do (
     where signtool >nul 2>nul
     if %errorlevel% neq 0 if exist %%V (
         echo Using %%V
         call %%V
     )
-)   
+)
 
 where signtool >nul 2>nul
 if %errorlevel% neq 0 (
