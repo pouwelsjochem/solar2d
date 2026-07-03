3441f4e068cd4cd3522b3b96410650c057e818d6
Jochem Pouwels <pouwelsjochem@gmail.com>
2026-07-03 11:44:29 +0200

    Remove Windows (Win32) build functionality from Simulator
    This commit removes all remaining native Windows (Win32) application build capabilities from the Corona Simulator.
    
    This aligns with the strategy of centralizing native builds through the external `CoronaBuilder` and streamlines the simulator to focus solely on project execution and debugging.
    
    Specifically, this includes:
    - Deletion of Win32-specific app packager classes and their associated build logic.
    - Removal of all build-related project settings (version, copyright, company name, etc.) from `CoronaProject`.
    - Elimination of associated UI elements, dialogs, string resources, and example icons used for Win32 builds.

--- platform/windows/Corona.Simulator/CoronaProject.cpp
+++ platform/windows/Corona.Simulator/CoronaProject.cpp
@@ -8,17 +8,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
-#include "Simulator.h"
-#include "SimulatorView.h"
 
-#include <ctype.h>  // _istalpha()
-#include <wincrypt.h>  // CryptProtectData(), CryptUnprotectData()
-
-#include "WinString.h"
-
-#include "Core/Rtt_Build.h"
-#include "Core/Rtt_String.h"
-#include "Rtt_PlatformAppPackager.h"
 #include "CoronaProject.h"
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -44,9 +34,6 @@ CCoronaProject::~CCoronaProject(void)
 void
 CCoronaProject::Init( CString sPath )
 {
-	// Fetch a pointer to the Corona Simulator's main application instance.
-	auto appPointer = AfxGetApp();
-
 	// Store the path of the main.lua file which "should" include the file name.
     m_sPath = sPath;
 
@@ -68,80 +55,6 @@ CCoronaProject::Init( CString sPath )
 		 else
 			 m_sName = sDirectory.Right( sDirectory.GetLength() - i - 1);
 	}
-
-	// If an EXE file name is not assigned, then generate one via the application name.
-	if (m_sExeFileName.IsEmpty())
-	{
-		WinString appName;
-		appName.SetTCHAR(m_sName);
-		if (Rtt_StringEndsWithNoCase(appName.GetUTF8(), ".exe") == false)
-		{
-			appName.Append(".exe");
-		}
-		Rtt::String utf8EscapedExeFileName;
-		Rtt::PlatformAppPackager::EscapeFileName(appName.GetUTF8(), utf8EscapedExeFileName, false);
-		appName.SetUTF8(utf8EscapedExeFileName.GetString());
-		m_sExeFileName = appName.GetTCHAR();
-	}
-
-	// If the destinatin path is empty, then choose a default path.
-	if (m_sSaveDir.IsEmpty())
-	{
-		// First, attempt to fetch the last build path the used by another project.
-		m_sSaveDir = appPointer->GetProfileString(REGISTRY_SECTION, REGISTRY_BUILD_DIR);
-
-		// If we still don't have a path, then this is the first time the user is doing a build.
-		// Choose a nice directory under the system's Documents directory by default.
-		if (m_sSaveDir.IsEmpty())
-		{
-			::SHGetFolderPath(nullptr, CSIDL_MYDOCUMENTS, nullptr, 0, m_sSaveDir.GetBuffer(MAX_PATH));
-			m_sSaveDir.ReleaseBuffer();
-			if (m_sSaveDir.IsEmpty() == false)
-			{
-				TCHAR lastCharacter = m_sSaveDir.GetAt(m_sSaveDir.GetLength() - 1);
-				if ((lastCharacter != _T('\\')) && (lastCharacter != _T('/')))
-				{
-					m_sSaveDir.Append(_T("\\"));
-				}
-				m_sSaveDir.Append(_T("Corona Built Apps"));
-			}
-		}
-	}
-
-	// If this is a new project, then attempt to choose a default company name.
-	// Note: If the project's existing company name registry entry is blank, then respect that setting.
-	if (m_sCompanyName.IsEmpty() && !RegistryEntryExists(sSection, REGISTRY_COMPANY_NAME))
-	{
-		if (RegistryEntryExists(REGISTRY_SECTION, REGISTRY_COMPANY_NAME))
-		{
-			// Use the last built app's company name.
-			m_sCompanyName = appPointer->GetProfileString(REGISTRY_SECTION, REGISTRY_COMPANY_NAME);
-		}
-		else
-		{
-			// Attempt to fetch the company name registered to the Windows system.
-			// Note: This will only happen if an app was never built before or the simulator's preferences were deleted.
-			const TCHAR kRegistryKeyName[] = _T("Software\\Microsoft\\Windows NT\\CurrentVersion");
-			const TCHAR kRegistryEntryName[] = _T("RegisteredOrganization");
-			HKEY registryKeyHandle = nullptr;
-			::RegOpenKeyEx(HKEY_LOCAL_MACHINE, kRegistryKeyName, 0, KEY_READ | KEY_WOW64_64KEY, &registryKeyHandle);
-			if (registryKeyHandle)
-			{
-				DWORD valueType = REG_NONE;
-				DWORD valueByteLength = 0;
-				::RegQueryValueEx(registryKeyHandle, kRegistryEntryName, nullptr, &valueType, nullptr, &valueByteLength);
-				if ((REG_SZ == valueType) && (valueByteLength > 0))
-				{
-					::RegQueryValueEx(
-							registryKeyHandle, kRegistryEntryName, nullptr, &valueType,
-							(LPBYTE)m_sCompanyName.GetBuffer(128), &valueByteLength);
-					m_sCompanyName.ReleaseBuffer();
-					m_sCompanyName.Trim();
-				}
-				::RegCloseKey(registryKeyHandle);
-			}
-		}
-	}
 }
 
 // Save - use path as registry section, save values
@@ -155,11 +68,6 @@ CCoronaProject::Save()
 
 	// Save settings to the project's registry key.
     RegistryPut( sPath );
-
-	// Save last used build path and company name to the application's global "Preferences" registry key.
-	// To be used as defaults for new projects in the future.
-	AfxGetApp()->WriteProfileString(REGISTRY_SECTION, REGISTRY_BUILD_DIR, m_sSaveDir);
-	AfxGetApp()->WriteProfileString(REGISTRY_SECTION, REGISTRY_COMPANY_NAME, m_sCompanyName);
 }
 
 // RegistryGet - read each saved value from the given section
@@ -170,12 +78,6 @@ CCoronaProject::RegistryGet( CString sSection )
     CWinApp *pApp = AfxGetApp();
 
     m_sName = pApp->GetProfileString( sSection, REGISTRY_NAME, REGISTRY_NAME_DEFAULT );
-    m_sWin32VersionString = pApp->GetProfileString( sSection, REGISTRY_WIN32_VERSION_STRING, REGISTRY_VERSION_STRING_DEFAULT );
-    m_sSaveDir = pApp->GetProfileString( sSection, REGISTRY_SAVEDIR, REGISTRY_SAVEDIR_DEFAULT );
-	m_sCopyright = pApp->GetProfileString( sSection, REGISTRY_COPYRIGHT );
-	m_sCompanyName = pApp->GetProfileString( sSection, REGISTRY_COMPANY_NAME );
-	m_sAppDescription = pApp->GetProfileString( sSection, REGISTRY_APP_DESCRIPTION );
-	m_sExeFileName = pApp->GetProfileString( sSection, REGISTRY_EXE_FILE_NAME );
 }
 
 // RegistryPut - save each value to the given section
@@ -185,30 +87,6 @@ CCoronaProject::RegistryPut( CString sSection )
     CWinApp *pApp = AfxGetApp();
 
     pApp->WriteProfileString( sSection, REGISTRY_NAME, m_sName );
-    pApp->WriteProfileString( sSection, REGISTRY_WIN32_VERSION_STRING, m_sWin32VersionString );
-    pApp->WriteProfileString( sSection, REGISTRY_SAVEDIR, m_sSaveDir );
-    pApp->WriteProfileString( sSection, REGISTRY_COPYRIGHT, m_sCopyright );
-    pApp->WriteProfileString( sSection, REGISTRY_COMPANY_NAME, m_sCompanyName );
-    pApp->WriteProfileString( sSection, REGISTRY_APP_DESCRIPTION, m_sAppDescription );
-    pApp->WriteProfileString( sSection, REGISTRY_EXE_FILE_NAME, m_sExeFileName );
-}
-
-bool
-CCoronaProject::RegistryEntryExists( CString sSection, CString sEntryName )
-{
-	bool wasEntryFound = false;
-	HKEY registryKeyHandle = nullptr;
-	::RegOpenKeyEx(AfxGetApp()->GetAppRegistryKey(), sSection, 0, KEY_READ, &registryKeyHandle);
-	if (registryKeyHandle)
-	{
-		auto result = ::RegQueryValueEx(registryKeyHandle, sEntryName, nullptr, nullptr, nullptr, nullptr);
-		if (ERROR_SUCCESS == result)
-		{
-			wasEntryFound = true;
-		}
-		::RegCloseKey(registryKeyHandle);
-	}
-	return wasEntryFound;
 }
 
 // RemoveMainLua - Returns string with \\main.lua removed, if present.
--- platform/windows/Corona.Simulator/CoronaProject.h
+++ platform/windows/Corona.Simulator/CoronaProject.h
@@ -9,27 +9,13 @@
 
 #pragma once
 
-#include "Rtt_TargetDevice.h"
-
-
 // CCoronaProjec
 
 // Registry entry names.
 #define REGISTRY_NAME _T("Name")
-#define REGISTRY_WIN32_VERSION_STRING _T("Win32VersionString")
-#define REGISTRY_PACKAGE _T("Package")
-#define REGISTRY_SAVEDIR _T("SaveDir")
-#define REGISTRY_BUILD_DIR _T("BuildDir")
-#define REGISTRY_COPYRIGHT _T("Copyright")
-#define REGISTRY_COMPANY_NAME _T("CompanyName")
-#define REGISTRY_APP_DESCRIPTION _T("AppDescription")
-#define REGISTRY_EXE_FILE_NAME _T("ExeFileName")
 
 // Registry defaults for each individual project.
 #define REGISTRY_NAME_DEFAULT _T("")
-#define REGISTRY_VERSION_STRING_DEFAULT _T("1.0.0")
-#define REGISTRY_PACKAGE_DEFAULT _T("")
-#define REGISTRY_SAVEDIR_DEFAULT _T("")
 
 ///////////////////////////////////////////////////////////////////////////////
 // CCoronaProject
@@ -54,38 +40,11 @@ public:
 	CString GetName()  { return m_sName; }
 	void SetName( CString sName ) { m_sName = sName; }
 
-	CString GetWin32VersionString() { return m_sWin32VersionString; }
-	void SetWin32VersionString(const CString& value) { m_sWin32VersionString = value; }
-
-	CString GetSaveDir() { return m_sSaveDir; }
-	void SetSaveDir( CString sPath ) { m_sSaveDir = sPath; }
-
-	CString GetCopyright() { return m_sCopyright; }
-	void SetCopyright(const CString& value) { m_sCopyright = value; }
-
-	CString GetCompanyName() { return m_sCompanyName; }
-	void SetCompanyName(const CString& value) { m_sCompanyName = value; }
-
-	CString GetAppDescription() { return m_sAppDescription; }
-	void SetAppDescription(const CString& value) { m_sAppDescription = value; }
-
-	CString GetExeFileName() { return m_sExeFileName; }
-	void SetExeFileName(const CString& value) { m_sExeFileName = value; }
-
 protected:
     void RegistryGet( CString sSection );
     void RegistryPut( CString sSection );
-	bool RegistryEntryExists( CString sSection, CString sEntryName );
 
 protected:
     CString m_sPath;  // full path, incl. main.lua
     CString m_sName;  // default dir name
-	CString m_sWin32VersionString;
-    CString m_sSaveDir;
-    CString m_sTargetOS;
-	CString m_sCopyright;
-	CString m_sCompanyName;
-	CString m_sAppDescription;
-	CString m_sExeFileName;
 };
-
